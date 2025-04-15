5a2e7795130586f853868191f34bee37fd7972b1
05Ash <wuxiatranslators@gmail.com>
2025-04-15 14:40:59 +0530

    Turtle Crossing final

--- python/Day_23_Turtle_Crossing_Game/main.py
+++ python/Day_23_Turtle_Crossing_Game/main.py
@@ -8,27 +8,31 @@ from gamescreen import highway_painter, Level_Writer
 
 Coordinate = namedtuple("Coordinate", ["x", "y"])
 FIELD = Coordinate(300, 300)
-#Create Gamescreen
+#Create Gamescreen, definte its size, paints it, stops the animation, and activates listeing of the screen
 gamescreen = Screen()
 gamescreen.setup(2*FIELD.x, 2*FIELD.y)
 gamescreen.bgcolor("black")
 gamescreen.tracer(0)
 gamescreen.listen()
 
+#Create a turtle to draw on the gamescreen, and initiate one to write the level on the screen
 painter = highway_painter(FIELD)
 level_writer = Level_Writer()
 
-#Create Turtle
+#Initiate a player for the game
 user = Player(FIELD.y)
-#Create Cars
+
+#Defines the number of cars present at one time on the screen and creates an array of them
 no_of_cars =30
 cars = [Car(FIELD) for _ in range(no_of_cars)]
-#Randomize Cars
-game_status = True
-cars_moved = []
-i=0
-counter = 0
 
+#Defines the require variables for the game
+game_status = True #True as long as the player has not crashed
+cars_moved = [] #A variable to star which cars has moved from the array of cars
+i=0 #index variable to move through list of cars
+counter = 0 # counter to make sure the cars moved one after another
+
+#A loop for the cars to move alternatively, closes after all cars have moved
 while i < no_of_cars:
     if counter == 0:
         cars[i].movement()
@@ -36,18 +40,11 @@ while i < no_of_cars:
         i += 1
     for car in cars_moved:
         car.movement()
-    # for car in cars:
-    #     car.movement()
 
-    counter = (counter+1)%5
-#Listening keys for the Turtle to move up
-#If turtle reaches the top, increase the speed
-#Level calculator
-#If turtle is hit by the car, game over
-level = 0
+level = 1 #Variable to store the level of the game
 while game_status:
-    potential_collision = []
     player_lane = user.ycor()
+    #Loop to move the cars with every iteration and check if the user has not collided with the cars
     for car in cars_moved:
         car.movement(level)
         if (player_lane == car.ycor()
@@ -55,14 +52,21 @@ while game_status:
             or player_lane - 40 == car.ycor()):
             if user.distance(car) < 40:
                 game_status = False
+
+    #Slow down the looping sequence
     sleep(0.1)
+
+    #Commands to act how the player moves across the screen
     gamescreen.onkey(user.move_up, "Up")
     gamescreen.onkey(user.move_down, "Down")
     gamescreen.update()
+
+    #Check if the user has reached the finish line
     if user.reached_finish_line():
         level += 1
         user.reset()
-    level_writer.writer(level+1)
+    level_writer.writer(level)
 
+#Writes game over once user is hit by the car
 level_writer.over()
 gamescreen.exitonclick()
