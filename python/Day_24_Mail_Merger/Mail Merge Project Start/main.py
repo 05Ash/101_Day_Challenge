ca0e6da99af1b16ef6ff6a1b8e9be8e1bbeb8c41
05Ash <wuxiatranslators@gmail.com>
2025-04-17 20:15:28 +0530

    Day 24

---
+++ python/Day_24_Mail_Merger/Mail Merge Project Start/main.py
@@ -0,0 +1,23 @@
+#TODO: Create a letter using starting_letter.txt
+import os
+script_dir = os.path.dirname(__file__)
+file_path_letter = os.path.join(script_dir, "./Input/Letters/starting_letter.txt")
+with open(file_path_letter, "r") as file:
+    letter_template = file.read()
+file_path_names = os.path.join(script_dir, "./Input/Names/invited_names.txt")
+with open(file_path_names, "r") as file:
+    names = file.read()
+for name in names.split("\n"):
+    file_path_write = os.path.join(script_dir, "./Output/ReadyToSend", name)
+    content = letter_template.replace("[name]", name)
+    with open(file_path_write, "w") as file:
+        file.write(content)
+
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+
+
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
