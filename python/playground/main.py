415362c3516340d7405e78d730135a6b83e2f0cc
05Ash <wuxiatranslators@gmail.com>
2025-08-14 08:17:47 +0530

    Fast Api round 2

--- python/playground/main.py
+++ python/playground/main.py
@@ -1,62 +1,19 @@
 # Import FastAPI
-import enum
-from typing import Optional
-from fastapi import FastAPI, Response,status, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, status, HTTPException
+from fastapi.responses import JSONResponse
+from h11 import Request
+from controller.user_controller import router as user_router
 
 
 # Create app instance
 app = FastAPI()
 
-#Authenicating the post data
-class Post(BaseModel):
-    title: str
-    content: str
-    publish: bool = True
-    rating: Optional[int] = None
+@app.exception_handler(HTTPException)
+def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(status_code=exc.status_code, content = {"error": exc.detail, "status_code": exc.status_code})
 
-my_posts = [{"title":"title of post1", "content": "content of post 1", "id":1}, {"title": "favoirite foods", "content": "I like pizza", "id": 2}]
+@app.exception_handler(Exception)
+def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"error":"Internal Server Error", "details":str(exc)})
 
-#Finds a post based on id
-def find_post(id):
-    for post in my_posts:
-        if post["id"] == id:
-            return post
-
-#Finds the index of a post
-def find_index_post(id):
-    for i, post in enumerate(my_posts):
-        if post["id"] == id:
-            return i
-
-# Define a route using the @app.get decorator
-@app.get("/")
-def read_root():
-    return {"Data": my_posts}
-
-@app.post("/posts", status_code = status.HTTP_201_CREATED)
-def create_posts(post : Post):
-    new_post = post.model_dump()
-    new_post["id"] = new_post.get("id",my_posts[-1]["id"]) + 1
-    my_posts.append(new_post)
-
-    return {"Data" : "Successfully published the post"}
-
-@app.get("/posts/{id}")
-def get_post(id : int, response : Response):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail = f"post with id: {id} was not found")
-
-    return {"post_detail": post}
-
-
-@app.delete("/posts/{id}", status_code = status.HTTP_202_ACCEPTED)
-
-def delete_post(id : int):
-    index = find_index_post(id)
-    print(index)
-    if index:
-        my_posts.pop(index)
-    return {"message": "post successfully deleted"}
+app.include_router(user_router)
